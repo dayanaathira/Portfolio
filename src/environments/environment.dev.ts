0c552417ee90338e2ff53c1b05d2eb4e0365faaf
Dayana Athira <dayana@tmrnd.com.my>
2026-04-17 11:12:46 +0800

    chore: stop tracking environment files with real URLs

--- src/environments/environment.dev.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://dev.api.dayana.cloud/api-arkspace/v1',
-};
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://dayana.cloud/api-arkspace/v1',
-};
